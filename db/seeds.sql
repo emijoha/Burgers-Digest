ec963b175647bd3feb16902ba00bd543d545ad8b
emi <ejhartline@gmail.com>
2020-05-06 22:44:07 -0400

    finished schema and seeds .sql files, created database in mysql workbench

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,12 @@
--- insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name, devoured)
+VALUES  
+    ("Gourdon-Hamsey Burger (comes with squash & ham)"), 
+    ("Let's Give 'Em Something Shiitake 'Bout Burger"),
+    ("Chorizo Your Own Adventure Burger"),
+    ("Poblano Picasso Burger"),
+    ("Eggers Can't Be Cheesers (with fried egg & cheese)"),
+    ("Beets of Burden Burger (served with too many beets)"),
+    ("Charbroil Fair Burger (comes with parsley, sage, rosemary, and thyme)"),
+    ("Don't You Four Cheddar 'Bout Me Burger (comes with four kinds of cheddar)"),
+    ("Sgt. Poblano Pepper Lonely Artichoke Hearts Club Burger"),
+    ("Home for the Challah-Days Burger (comes on a challah roll)");
