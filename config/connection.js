06321839b340ea510677350624210ebc716e470e
emi <ejhartline@gmail.com>
2020-05-07 22:09:05 -0400

    orm.js done, with sql statement functions for selectAll, insertOne, updateOne. ORM exported

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,23 @@
-// * Inside the `connection.js` file, setup the code to connect Node to MySQL.
+// Set up MySQL connection.
+const mysql = require("mysql");
+const password = require("../private/password");
 
-// * Export the connection.
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "burgers_db"
+  });
+  
+  // Make connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,112 @@
-// * Import (require) `connection.js` into `orm.js`
+// Import MySQL connection.
+var connection = require("../config/connection.js");
 
-// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+// ---------------------------------------------------------------------------------------------------------
+// MYSQL SYNTAX HELPER FUNCTIONS
+// ---------------------------------------------------------------------------------------------------------
 
-//   * `selectAll()`
-//   * `insertOne()`
-//   * `updateOne()`
+// Creates an array of question marks (as many as 'num') and turns it into a string
+// Ex: printQuestionMarks(3) returns "?,?,?""
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+};
 
-// * Export the ORM object in `module.exports`.
+// Converts object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
+// ---------------------------------------------------------------------------------------------------------
+// ORM OBJECT WITH SQL STATEMENT FUNCTIONS
+// ---------------------------------------------------------------------------------------------------------
+
+const orm = {
+
+  selectAll: function(tableInput, cb) {
+
+    const queryString = "SELECT * FROM " + tableInput + ";";
+
+    connection.query(queryString, function(err, result) {
+
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+
+    });
+  },
+
+  insertOne: function(table, columns, values, cb) {
+
+    const queryString = "INSERT INTO " + table;
+          queryString += " (";
+          queryString += columns.toString();
+          queryString += ") ";
+          queryString += "VALUES (";
+          queryString += printQuestionMarks(values.length);
+          queryString += ") ";
+    
+    console.log(queryString);
+
+    connection.query(queryString, values, function(err, result) {
+
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+
+    });
+  },
+
+  // objColVals ex: {col1: val1, col2: val2}
+  updateOne: function(table, objColVals, condition, cb) {
+
+    const queryString = "UPDATE " + table;
+          queryString += " SET ";
+          queryString += objToSql(objColVals);
+          queryString += " WHERE ";
+          queryString += condition;
+
+    console.log(queryString);
+
+    connection.query(queryString, function(err, result) {
+
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+
+    });
+  }
+
+};
+
+// Export ORM
+module.exports = orm;
